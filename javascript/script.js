86ec1dfcf34c5c988f05d5d42041ebe0d7a9a656
letyresina <leticiaresina@gmail.com>
2023-05-04 19:03:09 -0300

    Ajuste feito no scroll, agora tudo funcionando

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,6 @@
-var scrollCount = 0;
-document.getElementById('scroll-icon').addEventListener('click',function(){
-  window.requestAnimationFrame(function(){
-        scrollCount = window.pageYOffset;
-   scrollDown() 
-  });
-});
-document.getElementById('scroll-botao').addEventListener('click',function(){
-  window.requestAnimationFrame(function(){
-        scrollCount = window.pageYOffset;
-   scrollDown() 
-  });
-});
+const btnScroll = document.getElementById('scroll-botao');
+const secaoAlvo = document.getElementById('conteudo-home');
 
-function scrollDown(){
-  scrollCount += 25;
-  if (window.pageYOffset < document.getElementById('scroll').clientHeight){
-  window.scrollTo(0,scrollCount);
-  window.requestAnimationFrame(function(){
-    scrollDown();
-  });
-  }
-  else {
-  }
-};
-
-window.onscroll = function(){
-  if (window.pageYOffset > 500) {
-        document.getElementById('conteudo').classList.add('fade-in-left')
-       window.setTimeout(function(){
-          document.getElementById('conteudo').classList.add('fade-in-bottom')
-       },500)
-  }
-}
+btnScroll.addEventListener('click', function() {
+  secaoAlvo.scrollIntoView({ behavior: 'smooth' });
+});
