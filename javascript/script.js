fec35d1bb61f9942f3e7510636520a7e4c5c996d
letyresina <leticiaresina@gmail.com>
2023-05-03 21:03:24 -0300

    Feito o scrolldown (falta arrumar alguns detalhes)

---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+var scrollCount = 0;
+document.getElementById('scroll-icon').addEventListener('click',function(){
+  window.requestAnimationFrame(function(){
+        scrollCount = window.pageYOffset;
+   scrollDown() 
+  });
+});
+document.getElementById('scroll-botao').addEventListener('click',function(){
+  window.requestAnimationFrame(function(){
+        scrollCount = window.pageYOffset;
+   scrollDown() 
+  });
+});
+
+function scrollDown(){
+  scrollCount += 25;
+  if (window.pageYOffset < document.getElementById('scroll').clientHeight){
+  window.scrollTo(0,scrollCount);
+  window.requestAnimationFrame(function(){
+    scrollDown();
+  });
+  }
+  else {
+  }
+};
+
+window.onscroll = function(){
+  if (window.pageYOffset > 500) {
+        document.getElementById('conteudo').classList.add('fade-in-left')
+       window.setTimeout(function(){
+          document.getElementById('conteudo').classList.add('fade-in-bottom')
+       },500)
+  }
+}
