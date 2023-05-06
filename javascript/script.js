7014d2a3d892df06a99119f403261c8b409ea41c
letyresina <leticiaresina@gmail.com>
2023-05-06 17:32:05 -0300

    Feito a tela de cadastro sem nenhuma validação

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,22 @@ btnScroll.addEventListener('click', function() {
   secaoAlvo.scrollIntoView({ behavior: 'smooth' });
 });
 
-function alertaBotao(){
+
+//Função sujeitas a mudanças futuras
+function alertaBotaoAmelie(){
   alert("Amélie em construção")
+}
+
+function alertaBotaoCadastro(){
+  
+  let nomeUsuario = document.getElementsById("emailUsuario").value;
+  let senhaUsuario = document.getElementsById("senhaUsuario").value;
+
+  // Pequena validação, sujeita a mudanças
+  if (nomeUsuario == "" || senhaUsuario == ""){
+    alert("Por favor, preencha todos os campos solicitados!")
+  }else{ 
+    alert("Seja bem vindo(a)!")
+  }
+
 }
