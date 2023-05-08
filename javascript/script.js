878b57710e00459001089a9edb4d90c144e8ff8e
letyresina <leticiaresina@gmail.com>
2023-05-08 19:50:15 -0300

    fazer ajustes em login dps

--- javascript/script.js
+++ javascript/script.js
@@ -15,16 +15,7 @@ function alertaBotaoAmelie(){
   alert("Amélie em construção")
 }
 
+// Tentar fazer validação depois
 function alertaBotaoCadastro(){
-  
-  let nomeUsuario = document.getElementsById("emailUsuario").value;
-  let senhaUsuario = document.getElementsById("senhaUsuario").value;
-
-  // Pequena validação, sujeita a mudanças
-  if (nomeUsuario == "" || senhaUsuario == ""){
-    alert("Por favor, preencha todos os campos solicitados!")
-  }else{ 
     alert("Seja bem vindo(a)!")
-  }
-
 }
